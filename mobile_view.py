1d101584aa9aa1b0ccc2344efaa04b2538588199
techdoc-kabs <techdoc.kabs@gmail.com>
2025-05-16 15:39:33 +0300

    Update mobile_view.py

--- mobile_view.py
+++ mobile_view.py
@@ -5,8 +5,8 @@ import LogIn, SignUp
 import student_tool_page
 import streamlit as st
 import base64
-import os
-import get_help
+# import os
+# import get_help
 # from mysql.connector import Error
 import datetime
 from datetime import datetime
